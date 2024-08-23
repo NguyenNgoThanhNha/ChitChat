4680d0ddc374d7e8e1f22a23ab11addac2fdacec
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2024-08-23 20:48:36 +0700

    update profile

--- server/src/controllers/AuthController.js
+++ server/src/controllers/AuthController.js
@@ -104,8 +104,42 @@ const GetUserInFo = async (req, res) => {
     }
 }
 
+const UpdateProfile = async (req, res) => {
+    try {
+
+        const { userId } = req;
+        const { firstName, lastName, color } = req.body;
+        if (!firstName || !lastName) {
+            return res.status(400).json({ message: "FirstName and LastName are required!" })
+        }
+        const userData = await User.findByIdAndUpdate(userId, {
+            firstName, lastName, color, profileSetup: true
+        }, { new: true, runValidators: true })
+        console.log(userId)
+        if (!userData) {
+            return res.status(404).json({ message: "User with the given id not found." })
+        }
+
+        return res.status(200).json({
+            user: {
+                id: userData.id,
+                email: userData.email,
+                profileSetup: userData.profileSetup,
+                firstName: userData.firstName,
+                lastName: userData.lastName,
+                image: userData.image,
+                profileSetup: userData.profileSetup
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something went wrong" })
+    }
+}
+
 export default {
     SignUp,
     SignIn,
-    GetUserInFo
+    GetUserInFo,
+    UpdateProfile
 }
--- server/src/routers/Auth.Route.js
+++ server/src/routers/Auth.Route.js
@@ -6,5 +6,6 @@ const router = express.Router()
 router.get("/get-info", verifyToken, AuthController.GetUserInFo)
 router.post("/sign-up", AuthController.SignUp)
 router.post("/sign-in", AuthController.SignIn)
+router.post("/update-profile", verifyToken, AuthController.UpdateProfile)
 
 export default router;
