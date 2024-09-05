83bb923633f642f85cbeccbe777ba3c3ef5443df
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2024-09-05 14:12:27 +0700

    update schema

--- server/src/models/UserModel.js
+++ server/src/models/UserModel.js
@@ -2,7 +2,17 @@ import mongoose from "mongoose";
 import { genSalt, hash } from "bcrypt"
 const userSchema = new mongoose.Schema({
     email: { type: String, required: [true, "Email is Required"], unique: true },
-    password: { type: String, required: [true, "Password is Required"] },
+    password: {
+        type: String,
+        required: [true, "Password is Required"],
+        minlength: [6, "Password must be at least 6 characters"],
+        validate: {
+            validator: function (v) {
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,}$/.test(v);
+            },
+            message: "Password must contain at least one uppercase letter, one special character, and one lowercase letter."
+        }
+    },
     firstName: { type: String, required: false },
     lastName: { type: String, required: false },
     image: { type: String, required: false },
